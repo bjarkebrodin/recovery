fa81b4cf7508e8e763647baea3c75f76f90edb3a
Bjarke Brodin <bjarke@brodin.dk>
2024-05-27 21:28:54 +0200

    extract imports data

--- code/files.py
+++ code/files.py
@@ -1,8 +1,17 @@
+"""
+Fully typed module containing utilities to have more finegrained and OS 
+agnostic control of FS navigation without using pwd and cd like operations,
+this provides higher flexibility and abstracts away the different logical 
+approaches to crawling the file system.
+"""
+
+
 from os import listdir as _listdir, sep as _sep, stat as _stat
 from os.path import isdir as _isdir, exists as _exists, isfile as _isfile, expandvars as _expandvars, expanduser as _expanduser, islink as _islink, abspath as _abspath
 from stat import FILE_ATTRIBUTE_HIDDEN as _HIDDEN_BIT
-from inspect import stack as _stack
 from re import match as _match
+from typing import List, Iterable
+from typing_extensions import Self
 
 
 class Path():
@@ -10,7 +19,7 @@ class Path():
   Make it simpler to navigate and work with the file system the way we want!
   """
 
-  def __init__(self, pathstr):
+  def __init__(self, pathstr: str):
     path = _expandvars(pathstr)
     path = _expanduser(path)
     path = _abspath(path)
@@ -25,13 +34,12 @@ class Path():
 
 
   # These may change so we need to check each time
-  def isdir(self): return _isdir(self.path)
-  def isfile(self): return _isfile(self.path)
-  def islink(self): return _islink(self.path)
+  def isdir(self) -> bool: return _isdir(self.path)
+  def isfile(self) -> bool: return _isfile(self.path)
+  def islink(self) -> bool: return _islink(self.path)
+  def ishidden(self) -> bool: return self.name.startswith('.') or bool(_stat(self.path).st_file_attributes & _HIDDEN_BIT)
 
-  def ishidden(self): return self.name.startswith('.') or bool(_stat(self.path).st_file_attributes & _HIDDEN_BIT)
-
-  def children(self):
+  def children(self) -> Iterable[Self]:
 
     if not self.isdir():
       raise NotADirectoryError(f'{self.path} is not a directory')
@@ -39,16 +47,16 @@ class Path():
     return [Path(_sep.join([self.path, child])) for child in _listdir(self.path)]
 
 
-  def contents(self):
+  def contents(self) -> Iterable[str]:
 
     if self.isdir():
       raise IsADirectoryError(f'{self.path} is a directory')
 
-    with open(self.path) as f:
+    with open(self.path, 'r', encoding = 'utf-8') as f:
       return f.readlines()
 
   
-  def delve(self, include_hidden=False, follow_links=False, ignore=[]):
+  def delve(self, include_hidden: bool = False, follow_links: bool = False, ignore: List[str] = []) -> Iterable[Self]:
     include = not any([_match(i, self.path) is not None for i in ignore])
     follow = (not self.ishidden()) or include_hidden
 
@@ -61,4 +69,4 @@ class Path():
         for child in self.children():
           for result in child.delve(ignore=ignore):
             yield result
-      
+
--- code/main.py
+++ code/main.py
@@ -1,17 +1,88 @@
-
+from typing import List, TypeVar, Set, Callable
 from files import Path
+from re import match
+from os import sep
+
+
+###########################################################################################
+### JS UTILS: TODO: MOVE INTO OWN FILE
+
+# Inspired by Mircea Lungo's code found at:
+# https://colab.research.google.com/drive/1oe_TV7936Zmmzbbgq8rzqFpxYPX7SQHP#scrollTo=0ruTtX88Tb-w
+# accessed at 18:38 CET on 27-05-2024
+def extract_module(file: Path) -> str:
+  return file.path.split('src\\')[-1].replace('\\','.').replace('.js','').replace('.sc','')
+
+T = TypeVar('T')
+def flatten(l: List[List[T]]) -> List[T]:
+  return [x for y in l for x in y]
+
+def is_local_import(s: str) -> bool: 
+  return '.' in s
+
+def to_module_path_from_relative(file: Path) -> Path: 
+  return lambda s: Path(sep.join(file.path.split(sep)[:-1]) + sep + s.replace('/', sep).replace('.js', '') + '.js')
+
+def extract_js_imports(file: Path, ignore: List[str] = []) -> List[str]:
+
+  def _extractor(linefilter: Callable[[str], bool], linemapping: Callable[[str], str]) -> Callable[[], str]:
+    def __result_extractor():
+      return list(
+        map(extract_module,
+        map(to_module_path_from_relative(file),
+        filter(is_local_import,
+        map(linemapping,
+        filter(linefilter,
+        file.contents()))))))
+    
+    return __result_extractor
+
+  # Gets only singleline from imports such as "import component from 'module';"
+  _from_import_linefilter = lambda line: line.startswith('import') and ';' in  line and 'from' in line and not any([match(i, line) is not None for i in ignore])
+  _from_import_linemapping = lambda line: line.replace(';', '').replace('"', '').strip().replace('\n', '').split('from')[-1].strip()
+  _from_import_extractor = _extractor(_from_import_linefilter, _from_import_linemapping)
+    
+  # Gets only singleline flat imports such as "import './module';"
+  _flat_import_linefilter = lambda line: line.startswith('import') and ';' in line and 'from' not in line and not any([match(i, line) is not None for i in ignore])
+  _flat_import_linemapping = lambda line: line.replace(';', '').replace('"', '').strip().replace('\n', '').split(' ')[-1].strip()
+  _flat_import_extractor = _extractor(_flat_import_linefilter, _flat_import_linemapping)
+
+  from_imports = _from_import_extractor()
+  flat_imports = _flat_import_extractor()
+    
+  return from_imports + flat_imports
+###########################################################################################
+
 
 def main():
 
   # Can also just filter the path on only js afterwards but this speeds up crawl :)
-  ignore = [
+  ignore_files: List[str] = [
     '.*node_modules.*',
     '.*tex.*',
     '.*notes.*',
     '.*public.*',
+    '.*.json',
   ]
 
-  for p in Path('./src').delve(ignore=ignore):
-    print(p.path)
+  ignore_imports: List[str] = [
+    ".*.css",
+    ".*.json",
+  ]
+
+
+  codefiles: List[Path] = list(Path('./src').delve(ignore=ignore_files))
+  modules: Set[str] = set(map(extract_module, codefiles))
+  outrefs = None
+  increfs = None
+
+  for f in codefiles:
+    print(extract_module(f), '<-', extract_js_imports(f, ignore=ignore_imports))
+
+  # todo: abstract modules into a tree to work with abstraction hierarchy
+  # todo: look at truck
+  # todo: can generate UML by using semantic analysis, find libs for this!
+
+
 
 if __name__ == '__main__': main()
